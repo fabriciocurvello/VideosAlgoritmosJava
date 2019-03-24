67bd9c87f4f753872374123b37275d1a04cf36ab
Fabricio Curvello <fabricio.curvello@gmail.com>
2019-03-23 21:12:40 -0300

    Projeto inciciado e configuracoes da API efetuadas

---
+++ app/src/main/java/com/project/fabricio/videosalgoritmosjava/MainActivity.java
@@ -0,0 +1,16 @@
+package com.project.fabricio.videosalgoritmosjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String GOOGLE_API_KEY = "AIzaSyDS4cJoieUrK9yLUo39hV9EAMSkwR1fvQw";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
